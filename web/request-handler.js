a007c2bd3175990f531d16cc06ce0f3dac5766ee
misteryeo <andy.yeo@gmail.com>
2017-03-01 16:11:33 -0800

    Pass test to render index.html.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,18 @@
 var path = require('path');
+var url = require('url');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var parseUrl = url.parse(req.url);
+  console.log(archive.paths.siteAssets + '/index.html');
+  fs.readFile((archive.paths.siteAssets + '/index.html'), function(err, data) {
+    if (err) {
+      console.log('There\'s an error!');
+    } else {
+      res.end(data);
+    }
+  });
+  console.log(parseUrl.path);
 };
