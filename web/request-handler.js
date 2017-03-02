e3685f4bdc2c990e77b2420581d9f47a4e99d3a7
misteryeo <andy.yeo@gmail.com>
2017-03-01 16:58:40 -0800

    Saving progress.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,18 +1,41 @@
 var path = require('path');
 var url = require('url');
 var fs = require('fs');
+var qs = require('querystring');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
   var parseUrl = url.parse(req.url);
-  console.log(archive.paths.siteAssets + '/index.html');
-  fs.readFile((archive.paths.siteAssets + '/index.html'), function(err, data) {
-    if (err) {
-      console.log('There\'s an error!');
-    } else {
-      res.end(data);
-    }
-  });
+  console.log('This is our req method', req.method);
+  if (req.method === 'GET') {
+    fs.readFile((archive.paths.siteAssets + '/index.html'), function(err, data) {
+      if (err) {
+        console.log('There\'s an error!');
+      } else {
+        res.end(data);
+      }
+    });
+  } else if (req.method === 'POST') {
+    console.log('Post received!');
+
+    var data = '';
+
+    req.on('error', function(err) {
+      console.error(err);
+    }).on('data', function(chunk) {
+      data += chunk;
+    }).on('end', function() {
+      var dataObj = qs.parse(data);
+      fs.write(archive.paths.list, dataObj.url, function(err) {
+        if (err) {
+          console.log('Error!');
+        }
+        console.log('Murdered it!');
+      });
+    });
+    res.end('Hello, Craig');
+  }
+
   console.log(parseUrl.path);
 };
